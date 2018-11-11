8815552a8b30843c14b200f760671d0ec6666981
azad <arsyad.uniten@outlook.com>
2018-11-12 07:13:56 +0800

    added fontawesome

--- resources/views/component/details.blade.php
+++ resources/views/component/details.blade.php
@@ -28,7 +28,7 @@
 	}
 
 	body{
-		background: #e1e1e1;
+		background: #39445C;
 	}
 
 	/*.is-outlined{
@@ -41,55 +41,74 @@
 
 	}
 </style>
-<div class="container bg-white shadow-md mx-auto">
-	<div class="p-8">
-		<p class="text-xl font-bold text-grey-darkest">Research</p>
-		<p class="text-4xl font-bold -my-2">Research Title</p>
+<div class="container bg-white shadow-md mx-auto m-4 rounded-t-lg z-20">
+	<div class="flex p-8 brand-color rounded-t-lg">
+		<a class="pr-8" href="#"><i class="fas fa-2x fa-arrow-left text-black"></i></a>
+		<div>
+			<p class="text-xl font-bold text-grey-darkest">Research</p>
+			<p class="text-4xl font-bold -my-2 text-black">Research Title</p>
+		</div>
 	</div>
 	<div class="flex m-4 p-4">
 		<div class="bg-grey shadow-md rounded">
-			<p class="p-4">Description</p>
+			<p class="p-4 font-bold">Description</p>
 			<p class="p-4">Velit labore fugiat nulla est ut dolor consequat ut do voluptate. Ex sit culpa excepteur ad incididunt dolor adipisicing quis velit dolore consectetur nulla adipisicing. Occaecat sunt officia fugiat minim voluptate duis officia laborum nostrud. Elit magna magna et id culpa excepteur velit ad sed ut voluptate ut nulla excepteur consectetur ex adipisicing proident. Lorem ipsum deserunt id occaecat sunt velit sunt labore officia proident. Ea nulla ex laboris non reprehenderit do deserunt id deserunt pariatur laborum fugiat laborum est. Dolor veniam ea ex qui aliqua esse do quis fugiat esse occaecat in. Nisi irure velit magna ea ut in anim consectetur deserunt. Eu eiusmod quis anim culpa proident nisi velit est velit. Velit eu occaecat sit deserunt in ut ut dolor adipisicing anim tempor tempor. Mollit ad elit non enim tempor aliqua officia eu dolor sunt officia occaecat qui aute.</p>
 		</div>
 	</div>
-	<div class="flex m-4 p-4">
+	<div class="flex m-4 p-4 mb-0 pb-0">
 		<div class="flex-1 bg-grey shadow-md rounded m-4 ml-0">
-			<p class="p-4">Related Projects</p>
-			<p class="p-4">Dolore id non incididunt ut reprehenderit dolore excepteur ea deserunt sed exercitation ea consectetur in ea ut.</p>
-			<p class="p-4">Dolore id non incididunt ut reprehenderit dolore excepteur ea deserunt sed exercitation ea consectetur in ea ut.</p>
-			<p class="p-4">Dolore id non incididunt ut reprehenderit dolore excepteur ea deserunt sed exercitation ea consectetur in ea ut.</p>
-			<p class="p-4">Dolore id non incididunt ut reprehenderit dolore excepteur ea deserunt sed exercitation ea consectetur in ea ut.</p>
-			<p class="p-4">Dolore id non incididunt ut reprehenderit dolore excepteur ea deserunt sed exercitation ea consectetur in ea ut.</p>
-			<p class="p-4">Dolore id non incididunt ut reprehenderit dolore excepteur ea deserunt sed exercitation ea consectetur in ea ut.</p>
+			<p class="p-4 font-bold">Related Projects</p>
+			<ol>
+				<li class="p-4">Dolore id non incididunt ut reprehenderit dolore excepteur ea deserunt sed exercitation ea consectetur in ea ut.</li>
+				<li class="p-4">Dolore id non incididunt ut reprehenderit dolore excepteur ea deserunt sed exercitation ea consectetur in ea ut.</li>
+				<li class="p-4">Dolore id non incididunt ut reprehenderit dolore excepteur ea deserunt sed exercitation ea consectetur in ea ut.</li>
+				<li class="p-4">Dolore id non incididunt ut reprehenderit dolore excepteur ea deserunt sed exercitation ea consectetur in ea ut.</li>
+				<li class="p-4">Dolore id non incididunt ut reprehenderit dolore excepteur ea deserunt sed exercitation ea consectetur in ea ut.</li>
+				<li class="p-4">Dolore id non incididunt ut reprehenderit dolore excepteur ea deserunt sed exercitation ea consectetur in ea ut.</li>
+			</ol>
 		</div>
 		<div class="flex-1 bg-grey shadow-md rounded m-4 mr-0">
-			<p class="p-4">Publications</p>
-			<p class="p-4">Labore exercitation laborum proident sint id elit in qui aliqua.</p>
-			<p class="p-4">Labore exercitation laborum proident sint id elit in qui aliqua.</p>
-			<p class="p-4">Labore exercitation laborum proident sint id elit in qui aliqua.</p>
-			<p class="p-4">Labore exercitation laborum proident sint id elit in qui aliqua.</p>
-			<p class="p-4">Labore exercitation laborum proident sint id elit in qui aliqua.</p>
+			<p class="p-4 font-bold">Publications</p>
+			<ol>
+				<li class="p-4">Dolore id non incididunt ut reprehenderit dolore excepteur ea deserunt sed exercitation ea consectetur in ea ut.</li>
+				<li class="p-4">Dolore id non incididunt ut reprehenderit dolore excepteur ea deserunt sed exercitation ea consectetur in ea ut.</li>
+				<li class="p-4">Dolore id non incididunt ut reprehenderit dolore excepteur ea deserunt sed exercitation ea consectetur in ea ut.</li>
+				<li class="p-4">Dolore id non incididunt ut reprehenderit dolore excepteur ea deserunt sed exercitation ea consectetur in ea ut.</li>
+				<li class="p-4">Dolore id non incididunt ut reprehenderit dolore excepteur ea deserunt sed exercitation ea consectetur in ea ut.</li>
+				<li class="p-4">Dolore id non incididunt ut reprehenderit dolore excepteur ea deserunt sed exercitation ea consectetur in ea ut.</li>
+			</ol>
 		</div>
 	</div>
-	<div class="flex m-4 mt-0 p-4">
+	<div class="flex m-4 mt-0 p-4 pt-0">
 		<div class="flex-1 bg-grey shadow-md rounded m-4 ml-0">
-			<p class="p-4">Related Projects</p>
-			<p class="p-4">Dolore id non incididunt ut reprehenderit dolore excepteur ea deserunt sed exercitation ea consectetur in ea ut.</p>
-			<p class="p-4">Dolore id non incididunt ut reprehenderit dolore excepteur ea deserunt sed exercitation ea consectetur in ea ut.</p>
-			<p class="p-4">Dolore id non incididunt ut reprehenderit dolore excepteur ea deserunt sed exercitation ea consectetur in ea ut.</p>
-			<p class="p-4">Dolore id non incididunt ut reprehenderit dolore excepteur ea deserunt sed exercitation ea consectetur in ea ut.</p>
-			<p class="p-4">Dolore id non incididunt ut reprehenderit dolore excepteur ea deserunt sed exercitation ea consectetur in ea ut.</p>
-			<p class="p-4">Dolore id non incididunt ut reprehenderit dolore excepteur ea deserunt sed exercitation ea consectetur in ea ut.</p>
+			<p class="p-4 font-bold">Collaborators</p>
+			<ol>
+				<li class="p-4">Dolore id non incididunt ut reprehenderit dolore excepteur ea deserunt sed exercitation ea consectetur in ea ut.</li>
+				<li class="p-4">Dolore id non incididunt ut reprehenderit dolore excepteur ea deserunt sed exercitation ea consectetur in ea ut.</li>
+				<li class="p-4">Dolore id non incididunt ut reprehenderit dolore excepteur ea deserunt sed exercitation ea consectetur in ea ut.</li>
+				<li class="p-4">Dolore id non incididunt ut reprehenderit dolore excepteur ea deserunt sed exercitation ea consectetur in ea ut.</li>
+				<li class="p-4">Dolore id non incididunt ut reprehenderit dolore excepteur ea deserunt sed exercitation ea consectetur in ea ut.</li>
+				<li class="p-4">Dolore id non incididunt ut reprehenderit dolore excepteur ea deserunt sed exercitation ea consectetur in ea ut.</li>
+			</ol>
 		</div>
 		<div class="flex-1 bg-grey shadow-md rounded m-4 mr-0">
-			<p class="p-4">Publications</p>
-			<p class="p-4">Labore exercitation laborum proident sint id elit in qui aliqua.</p>
-			<p class="p-4">Labore exercitation laborum proident sint id elit in qui aliqua.</p>
-			<p class="p-4">Labore exercitation laborum proident sint id elit in qui aliqua.</p>
-			<p class="p-4">Labore exercitation laborum proident sint id elit in qui aliqua.</p>
-			<p class="p-4">Labore exercitation laborum proident sint id elit in qui aliqua.</p>
+			<p class="p-4 font-bold">Awards</p>
+			<ol>
+				<li class="p-4">Dolore id non incididunt ut reprehenderit dolore excepteur ea deserunt sed exercitation ea consectetur in ea ut.</li>
+				<li class="p-4">Dolore id non incididunt ut reprehenderit dolore excepteur ea deserunt sed exercitation ea consectetur in ea ut.</li>
+				<li class="p-4">Dolore id non incididunt ut reprehenderit dolore excepteur ea deserunt sed exercitation ea consectetur in ea ut.</li>
+				<li class="p-4">Dolore id non incididunt ut reprehenderit dolore excepteur ea deserunt sed exercitation ea consectetur in ea ut.</li>
+				<li class="p-4">Dolore id non incididunt ut reprehenderit dolore excepteur ea deserunt sed exercitation ea consectetur in ea ut.</li>
+				<li class="p-4">Dolore id non incididunt ut reprehenderit dolore excepteur ea deserunt sed exercitation ea consectetur in ea ut.</li>
+			</ol>
 		</div>
 	</div>
+</div>
+<div class="container mx-auto z-10 mb-8">
+	<a class="flex font-bold brand-color no-underline text-black p-6 md-4 shadow-md w-full text-2xl rounded-b-lg -my-4" href="#">
+		<span class="flex-1">Go to Project Website</span>
+		<i class="fas fa-arrow-right text-black"></i>
+	</a>
 </div>
 
 @endsection
--- resources/views/public/base.blade.php
+++ resources/views/public/base.blade.php
@@ -8,6 +8,7 @@
 	<link href="https://cdn.jsdelivr.net/npm/tailwindcss/dist/tailwind.min.css" rel="stylesheet">
 	<link rel="stylesheet" type="text/css" media="screen" href="https://cdn.rawgit.com/resir014/Clear-Sans-Webfont/v1.1.1/css/clear-sans.css">
 	<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
 	<style type="text/css">
 		body{
 	        font-family: "Clear Sans", "Helvetica Neue","sans-serif";
