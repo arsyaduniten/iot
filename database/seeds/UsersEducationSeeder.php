7dbc132eaf1f4a23aecd75d79831f32c11c7dda2
azad <arsyad.uniten@outlook.com>
2018-11-13 07:17:39 +0800

    hotdix

---
+++ database/seeds/UsersEducationSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UsersEducationSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        // DB::table('users')->insert([
+        //     'first_name' => "Sami",
+        //     'last_name' => "Hajjaj",
+        //     'password' => bcrypt('secret'),
+        // ]);
+    }
+}
